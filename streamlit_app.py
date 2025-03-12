02f42b944159e9b2aebeb60f41d4eab087f156ca
Cody Large <dlarge1@ewu.edu>
2025-03-12 01:51:09 -0700

    temp 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,11 @@ from src.classes import get_classes
 from src.oauth import get_login_url, get_google_info
 from src.firebase_config import create_user_if_not_exists, create_new_chat, fetch_chat_history, load_chat, update_chat_history, add_feedback
 from src.llama_utils import generate_clip_description, process_user_input, display_current_chat, generate_chat_title
-
+'''
 def main():
     st.set_page_config(page_title="CLIP Crop & Disease Detection", layout="wide")
     #classes = get_classes()
+    '''
     clicked_previous_chat = False  # temporary
     prompts = 0  # Track # of prompts
 
